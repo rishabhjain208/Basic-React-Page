e2ce329bece0e6dce970d9f97ec5990d291c291f
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-04-26 09:38:34 +0530

    Add files via upload

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+    return (
+        <div>
+            <nav>
+                <div className="logo">
+                    <img src="/img/brand_logo.png" alt="logo" />
+                </div>
+                <ul>
+                    <li href="#">Menu</li>
+                    <li href="#">Location</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+                </ul>
+                <button>login</button>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation
